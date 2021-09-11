b02a066e47a0293c6da5c11725d8fdafe0e387f3
Pasertcbs <khamphien808@gmail.com>
2021-09-12 03:24:04 +0700

    Implement delete functionality for the question

--- laravel/app/Question.php
+++ laravel/app/Question.php
@@ -21,18 +21,22 @@ class Question extends Model
     }
     public function getUrlAttribute()
     {
-        return route("questions.show", $this->id);
+        return route("questions.show", $this->slug);
     }
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
     }
-    public function getStatusAttribute(){
-        if ($this->answers > 0){
-                if ($this->best_answer_id){
-                    return "answered-accepted";
-                }
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
             return "answered";
         }
         return "unanswered";
+    }
+    public function getBodyHtmlAttribute(){
+        return \Parsedown::instance()->text($this->body);
     }
 }
