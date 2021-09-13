1ca75ff4a1481842c154a22e7c8df3ecf62a4667
Pasertcbs <khamphien808@gmail.com>
2021-09-13 12:14:00 +0700

    Design answers table schema and rename answers column to answers_count in the questions table

--- laravel/app/Question.php
+++ laravel/app/Question.php
@@ -7,10 +7,45 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+//    protected $fillable = ['title', 'body'];
+//
+//    public function user() {
+//
+//        return $this->belongsTo(User::class);
+//    }
+//
+//    public function setTitleAttribute($value)
+//    {
+//        $this->attributes['title'] = $value;
+//        $this->attributes['slug'] = str_slug($value);
+//    }
+//    public function getUrlAttribute()
+//    {
+//        return route("questions.show", $this->slug);
+//    }
+//    public function getCreatedDateAttribute(){
+//        return $this->created_at->diffForHumans();
+//    }
+//    public function getStatusAttribute()
+//    {
+//        if ($this->answers_count > 0) {
+//            if ($this->best_answer_id) {
+//                return "answered-accepted";
+//            }
+//            return "answered";
+//        }
+//        return "unanswered";
+//    }
+//    public function getBodyHtmlAttribute(){
+//        return \Parsedown::instance()->text($this->body);
+//    }
+//    public function answers(){
+//        $this->hasMany(Answer::class);
+//    }
+
     protected $fillable = ['title', 'body'];
 
     public function user() {
-
         return $this->belongsTo(User::class);
     }
 
@@ -19,16 +54,20 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
     }
+
     public function getUrlAttribute()
     {
         return route("questions.show", $this->slug);
     }
-    public function getCreatedDateAttribute(){
+
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
     }
+
     public function getStatusAttribute()
     {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if ($this->best_answer_id) {
                 return "answered-accepted";
             }
@@ -36,7 +75,16 @@ class Question extends Model
         }
         return "unanswered";
     }
-    public function getBodyHtmlAttribute(){
+
+    public function getBodyHtmlAttribute()
+    {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+        // $question->answers->count()
+        // foreach ($question->answers as $answer)
     }
 }
--- laravel/app/User.php
+++ laravel/app/User.php
@@ -37,4 +37,7 @@ class User extends Authenticatable
 //        return route("questions.show", $this->id);
         return '#';
     }
+    public function answers(){
+        return $this->hasMany(Answer::class);
+    }
 }
